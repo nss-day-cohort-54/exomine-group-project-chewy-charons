161afbd4fed05c80bde945957ddb495aba99be11
Stevie Hollander <steviehollander@icloud.com>
2022-02-14 13:18:40 -0600

    xomine&main

--- scripts/XO-Mine.js
+++ scripts/XO-Mine.js
@@ -1,5 +1,16 @@
 // this module provide everything to update the DOM
 // all the imports, all the HTML
 
+import { Facilities } from "./facilities"
+import { GovenorOptions } from "./governors"
 
+export const xoMine = () => {
+    return `
+    <h1>Solar System Mining Marketplace</h1>
+    <label class="choosegovenor" for="Choose a Govenor">Author</label>
+    ${GovenorOptions}
+    <label class="facilities" for="Choose a Facility">Author</label>
+    ${Facilities}
+    `
+}
 
