94af0d183419ee6fcd49ff243031891044f1ab5c
Gregory Haney <gregory.haney@gmail.com>
2022-02-14 14:34:03 -0600

    Minor updates

--- scripts/XO-Mine.js
+++ scripts/XO-Mine.js
@@ -2,15 +2,25 @@
 // all the imports, all the HTML
 
 import { Facilities } from "./facilities.js"
-import { GovenorOptions } from "./governors.js"
+import { GovernorOptions } from "./governors.js"
 
 export const xoMine = () => {
     let html = `
     <h1>Solar System Mining Marketplace</h1>
-    <label class="choosegovenor" for="choosegovenor">Choose a Govenor</label>
-    ${GovenorOptions()}
-    <label class="choosefacility" for="choosefacility">Choose a Facility</label>
-    ${Facilities()}
+
+    <label class="chooseGovernor" for="chooseGovernor">Choose a Governor</label>
+            ${GovernorOptions()}
+
+    <label class="chooseFacility" for="chooseFacility">Choose a Facility</label>
+             ${Facilities()}
+
+
+    <article>
+        
+    </article>
+
+
+
     `
     return html
 }
