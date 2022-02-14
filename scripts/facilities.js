f02fba8c85c93255bd1a164c37662829ad7c4ab0
Zach Dugger <ztd522@gmail.com>
2022-02-14 14:52:44 -0500

    divided labor, troubleshooting git puuls

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -1,21 +1,29 @@
-import { getFacilities } from "./database.js"
+import { getFacilities, setFacility } from "./database.js"
 
 //function that will iterate through facilities and format into html
-export const Facilities = () => {
-    const facilities = getFacilities()
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "facility") {
+            setFacility(parseInt(changeEvent.target.value))
+        }
+    }
+)
 
+const facilities = getFacilities()
+
+export const Facilities = () => {
     let html = `
     <select id="facility">
     <option value="">Choose Facility...</option>
 
-    ${facilities.map((facility) => (`<option value+"${facility.id}">${facility.name}</option>`)).join("")}
+    ${facilities.map((facility) => (`<option value="${facility.id}">${facility.name}</option>`)).join("")}
 
     </select> `
 
     return html
 
 }
-
 
 
 
