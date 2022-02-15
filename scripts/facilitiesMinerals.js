20bca3136e259fe43ba6d9d0fa5c550d3451d316
Christian Haggerty <captivationstation@gmail.com>
2022-02-15 11:04:53 -0600

    html changed

--- scripts/facilitiesMinerals.js
+++ scripts/facilitiesMinerals.js
@@ -1,27 +1,28 @@
-import { getFacilityMinerals, getMinerals } from "./database.js";
-import { Facilities } from "./facilities.js";
+// import { getFacilityMinerals, getMinerals } from "./database.js";
+// import { Facilities } from "./facilities.js";
 
-//we need to set facility.id === facilityminerals.facilityID
-//looking for a facility and using that to find infor
-export const findIds = (facMin) => {
-    const facilityMinerals = getFacilityMinerals()
-    const facilities = Facilities()
-    const minerals = getMinerals()
+// //we need to set facility.id === facilityminerals.facilityID
+// //looking for a facility and using that to find infor
+// export const findIds = (facMin) => {
 
-    const foundFacilties = facilities.find(
-        (facility) => {
-            return (facility.id === facMin.facilityId)
-        }
+//     const facilityMinerals = getFacilityMinerals()
+//     const facilities = Facilities()
+//     const minerals = getMinerals()
 
-    )
+//     const foundFacilties = facilities.find(
+//         (facility) => {
+//             return (facility.id === facMin.facilityId)
+//         }
 
-
-
-    const foundMinerals = minerals.find(
-        (mineral) => {
-            return (mineral.id === facMin.mineralId)
-        }
-    )
+//     )
 
 
-}
+
+//     const foundMinerals = minerals.find(
+//         (mineral) => {
+//             return (mineral.id === facMin.mineralId)
+//         }
+//     )
+
+
+// }
